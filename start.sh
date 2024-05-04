612581fa726ac3fbeb44068e7fb51ea7f4c7d196
Stefan Gatu <stefan.gatu@gmail.com>
2024-05-04 15:27:28 +0000

    first commit: docker deployment of chezz game

--- start.sh
+++ start.sh
@@ -7,10 +7,7 @@ if [ ! -d ./back/code/.git ]; then
   git clone https://github.com/sgatu/chezz-back ./back/code
 fi
 git -C ./back/code pull origin main
-echo "REDIS_HOST=redis" >> ./back/code/.env.dev
-echo "REDIS_PORT=6379" >> ./back/code/.env.dev
-echo "REDIS_PREFIX=chezz:" >> ./back/code/.env.dev
-echo "ALLOWED_DOMAIN=http://localhost:8025" >> ./back/code/.env.dev
+cat ./.env.back > ./back/code/.env.dev
 
 if [ ! -d ./front/code ]; then
    mkdir ./front/code
@@ -20,5 +17,5 @@ if [ ! -d ./front/code/.git ]; then
   git clone https://github.com/sgatu/chezz-front ./front/code
 fi
 git -C ./front/code pull origin main
-echo "PUB_API_BASE=http://localhost:8026" > ./front/code/.env.local
+cat ./.env.front > ./front/code/.env.local
 docker-compose up --build --force-recreate -d
